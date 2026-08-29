bc0ff17a6b7db337de2d8ee11d3326654d522fb4
Elman Farziyev <elmanfarziyevv@gmail.com>
2026-08-29 15:16:19 -0400

    Enable fully parallel test execution and retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,24 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
 
+  fullyParallel: true,
+
+  retries: process.env.CI ? 2 : 0,
+
   reporter: 'html',
+
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
 
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
   ],
 });
